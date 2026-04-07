5b1306426d31cb2d39a20026245daa1a2c5c4d86
Francisco <francisco2do1994@gmail.com>
2026-04-07 09:54:36 -0300

    Clase 6 (Parte1) - Implementando clases de Acceso a Datos

---
+++ Comun/ViewModels/ListadoPaginadoVMR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun.ViewModels
+{
+    public class ListadoPaginadoVMR<T>
+    {
+        public int cantidadTotal { get; set; }
+        public IEnumerable<T> elementos { get; set; }
+
+        public ListadoPaginadoVMR()
+        {
+            cantidadTotal = 0;
+            elementos = new List<T>();
+        }
+    }
+
+}
---
+++ Datos/DAL/MedicoDAL.cs
@@ -0,0 +1,19 @@
+﻿using Comun.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DAL
+{
+    public class MedicoDAL
+    {
+        public static ListadoPaginadoVMR<MedicoVMR> LeerTodo(int cantidad, int pagina, string textobusqueda)  //cantidad: cantidad de elementos por pagina
+        {
+            ListadoPaginadoVMR<MedicoVMR> resultado = new ListadoPaginadoVMR<MedicoVMR>();
+            return resultado;
+
+        }
+    }
+}
