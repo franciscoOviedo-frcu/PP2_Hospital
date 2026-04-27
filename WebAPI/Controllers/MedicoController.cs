924ca108a00374bbd92d213fe4b0c150b0fb5042
Francisco <francisco2do1994@gmail.com>
2026-04-27 09:53:39 -0300

    Clase 11 - Parte 1 - Creacion de un Controlador Web API para un CRUD

---
+++ WebAPI/Controllers/MedicoController.cs
@@ -0,0 +1,37 @@
+﻿using Comun.ViewModels;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class MedicoController : ApiController
+    {
+        public IHttpActionResult LeerTodo(int cantidad = 10, int pagina = 0, string textobusqueda = null)
+        {
+            return Ok(Logica.BLL.MedicoBLL.LeerTodo(cantidad, pagina, textobusqueda));
+        }
+
+
+        public IHttpActionResult LeerUno(long id)
+        {
+
+        }
+
+        public IHttpActionResult Crear(Medico Item)
+        {
+        }
+
+        public IHttpActionResult Actualizar(long id, MedicoVMR Item)
+        {
+        }
+
+        public IHttpActionResult Eliminar(List<long> ids)
+        {
+        }
+    }
+}
